940845efc3d3e4017f283e32f5c72e94918c2290
kylieainslie <ainslie.kylie@gmail.com>
2021-02-02 20:05:45 +0100

    postprocess age structured seir model output

---
+++ R/postprocess_age_struct_model_output.R
@@ -0,0 +1,66 @@
+#' Postprocess output from age-structured SEIR ODE model of vaccination 
+#' with 2 doses and delay to protection
+#' @param dat output from seir model as a data frame
+#' @return List of summary results
+#' @keywords vacamole
+#' @export
+postprocess_age_struct_model_output <- function(dat){
+
+  S <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "S[:digit:]")])
+  Shold_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Shold_1d[:digit:]")])
+  Sv_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Sv_1d[:digit:]")])
+  Shold_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Shold_2d[:digit:]")])
+  Sv_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Sv_2d[:digit:]")])
+  E <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "E[:digit:]")])
+  Ev_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Ev_1d[:digit:]")])
+  Ev_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Ev_2d[:digit:]")])
+  I <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "I[:digit:]")])
+  Iv_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Iv_1d[:digit:]")])
+  Iv_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Iv_2d[:digit:]")])
+  H <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "H[:digit:]")])
+  Hv_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Hv_1d[:digit:]")])
+  Hv_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Hv_2d[:digit:]")])
+  D <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "D[:digit:]")])
+  R <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "R[:digit:]")])
+  Rv_1d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_1d[:digit:]")])
+  Rv_2d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_2d[:digit:]")])
+  
+  rtn <- list(S = S,
+              Shold_1d = Shold_1d,
+              Sv_1d = Sv_1d,
+              Shold_2d = Shold_2d,
+              Sv_2d = Sv_2d,
+              E = E,
+              Ev_1d = Ev_1d,
+              Ev_2d = Ev_2d,
+              I = I,
+              Iv_1d = Iv_1d,
+              Iv_2d = Iv_2d,
+              H = H,
+              Hv_1d = Hv_1d,
+              Hv_2d = Hv_2d,
+              D = D,
+              R = R,
+              Rv_1d = Rv_1d,
+              Rv_2d = Rv_2d)
+  
+  return(rtn)
+}
