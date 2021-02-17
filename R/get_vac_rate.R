eecc1b26a1d3aa2c81f163dbef3d6bad7e36559c
kylieainslie <ainslie.kylie@gmail.com>
2021-02-17 12:27:09 +0100

    fix issues with NaN and 0 values

--- R/get_vac_rate.R
+++ R/get_vac_rate.R
@@ -20,10 +20,10 @@ get_vac_rate <- function(times, params){
   # pfizer
   pf_dose1 <- vac_schedule %>%
     select(date, pf_d1_1:pf_d1_9)
-  pf_dose1_cs <- cumsum(pf_dose1)
+  pf_dose1_cs <- cumsum(pf_dose1[,-1])
   pf_dose2 <- vac_schedule %>%
     select(date, pf_d2_1:pf_d2_9)
-  pf_dose2_cs <- cumsum(pf_dose2)
+  pf_dose2_cs <- cumsum(pf_dose2[,-1])
   
   # moderna
   # mo_dose1 <- vac_schedule %>%
@@ -34,38 +34,36 @@ get_vac_rate <- function(times, params){
   # astrazeneca
   az_dose1 <- vac_schedule %>%
     select(date, az_d1_1:az_d1_9)
-  az_dose1_cs <- cumsum(az_dose1)
+  az_dose1_cs <- cumsum(az_dose1[,-1])
   az_dose2 <- vac_schedule %>%
     select(date, az_d2_1:az_d2_9)
-  az_dose1_cs <- cumsum(az_dose2)
+  az_dose2_cs <- cumsum(az_dose2[,-1])
   
   # calculate composite VE
   time_point <- ifelse(floor(times) == 0, floor(times) + 1, floor(times))
   
-  total_dose1 <- unlist(pf_dose1_cs[time_point,-1] + 
-                          #mo_dose1[time_point,-1] + 
-                          az_dose1_cs[time_point,-1])
+  total_dose1 <- unlist(pf_dose1_cs[time_point,] + az_dose1_cs[time_point,])
   names(total_dose1) <- paste0("tot_",c(substr(names(total_dose1), 4, 7)))
-  total_dose2 <- unlist(pf_dose2_cs[time_point,-1] + 
-                          #mo_dose2[time_point,-1] + 
-                          az_dose2_cs[time_point,-1])
+  total_dose2 <- unlist(pf_dose2_cs[time_point,] + az_dose2_cs[time_point,])
   names(total_dose2) <- paste0("tot_",c(substr(names(total_dose2), 4, 7)))
-
-  comp_ve_dose1 <- unlist((pf_dose1_cs[time_point,-1]/total_dose1) * ve$pfizer[1] +
-                   #(mo_dose1[time_point,-1]/total_dose1) * ve$moderna[1] +
-                   (az_dose1_cs[time_point,-1]/total_dose1) * ve$astrazeneca[1])
-
-  comp_ve_dose2 <- unlist((pf_dose2_cs[time_point,-1]/total_dose2) * ve$pfizer[2] +
-                   #(mo_dose2[time_point,-1]/total_dose2) * ve$moderna[2] +
-                   (az_dose2_cs[time_point,-1]/total_dose2) * ve$astrazeneca[2])
   
-  delay_dose1 <- unlist((pf_dose1_cs[time_point,-1]/total_dose1) * delay$pfizer[1] +
-                            #(mo_dose1[time_point,-1]/total_dose1) * ve$moderna[1] +
-                            (az_dose1_cs[time_point,-1]/total_dose1) * delay$astrazeneca[1])
+  frac_pf_dose1 <- unlist(pf_dose1_cs[time_point,]/total_dose1)
+  frac_pf_dose1 <- ifelse(is.nan(frac_pf_dose1), 0, frac_pf_dose1)
+  frac_pf_dose2 <- unlist(pf_dose2_cs[time_point,]/total_dose2)
+  frac_pf_dose2 <- ifelse(is.nan(frac_pf_dose2), 0, frac_pf_dose2)
   
-  delay_dose2 <- unlist((pf_dose2_cs[time_point,-1]/total_dose2) * delay$pfizer[2] +
-                            #(mo_dose2[time_point,-1]/total_dose2) * ve$moderna[2] +
-                            (az_dose2_cs[time_point,-1]/total_dose2) * delay$astrazeneca[2])
+  frac_az_dose1 <- unlist(az_dose1_cs[time_point,]/total_dose1)
+  frac_az_dose1 <- ifelse(is.nan(frac_az_dose1), 0, frac_az_dose1)
+  frac_az_dose2 <- unlist(az_dose2_cs[time_point,]/total_dose2)
+  frac_az_dose2 <- ifelse(is.nan(frac_az_dose2), 0, frac_az_dose2)
+
+  comp_ve_dose1 <- frac_pf_dose1 * ve$pfizer[1] + frac_az_dose1 * ve$astrazeneca[1]
+  comp_ve_dose2 <- frac_pf_dose2 * ve$pfizer[2] + frac_az_dose2 * ve$astrazeneca[2]
+  
+  delay_dose1 <- frac_pf_dose1 * delay$pfizer[1] + frac_az_dose1 * delay$astrazeneca[1]
+  delay_dose1 <- ifelse(delay_dose1 == 0, 1, delay_dose1) # this prevents from deviding by 0 in the ODEs
+  delay_dose2 <- frac_pf_dose2 * delay$pfizer[2] + frac_az_dose2 * delay$astrazeneca[2]
+  delay_dose2 <- ifelse(delay_dose2 == 0, 1, delay_dose2) # this prevents from deviding by 0 in the ODEs
   
   # if(times >= t_ve_pf & times < t_ve_az){
   #   comp_ve_dose1 <- c(0,rep(ve$pfizer[1],8))
