1a546ac2cc4edadeff22470c457471782c368232
kylieainslie <ainslie.kylie@gmail.com>
2021-03-03 08:02:17 +0100

    tidying

--- inst/extdata/scripts/convert_vac_schedule.R
+++ inst/extdata/scripts/convert_vac_schedule.R
@@ -15,6 +15,8 @@ age_dist_10 <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463,
 n <- 17407585 # Dutch population size
 n_vec_10 <- n * age_dist_10
 
+# calculate vac coverage (up to 1 July 2021)
+
 # take the difference for each row
 # original
 vac_schedule_orig <- data.frame(diff(as.matrix(cum_vac_schedule_orig[-1,-1]))) %>%
@@ -84,141 +86,6 @@ vac_schedule_no2dose_new <- bind_rows(vac_schedule_orig_8March, vac_schedule_no2
 vac_schedule_delay3mo_new <- bind_rows(vac_schedule_orig_8March, vac_schedule_delay3mo) %>%
   filter(date > as.Date("2021-01-31")) %>%
   add_row(before_feb, .before = 1)
-
-#%>%
-  # mutate(pf_d1_9 = ifelse(pf_d1_9 == 0.0001, 0, pf_d1_9),
-  #        pf_d2_9 = ifelse(pf_d2_9 == 0.0001, 0, pf_d2_9),
-  #        mo_d1_9 = ifelse(mo_d1_9 == 0.0001, 0, mo_d1_9),
-  #        mo_d2_9 = ifelse(mo_d2_9 == 0.0001, 0, mo_d2_9),
-  #        az_d1_9 = ifelse(az_d1_9 == 0.0001, 0, az_d1_9),
-  #        az_d2_9 = ifelse(az_d2_9 == 0.0001, 0, az_d2_9))
-
-# re-allocate all second doses beyond 2 March 2021 to first doses
-# this results in some age groups with >100%
-# vac_schedule_no_second_doses <- vac_schedule %>%
-#   mutate(# pfizer dose 1
-#          pf_d1_1 = ifelse(date > as.Date("2021-03-08"), pf_d1_1 + pf_d2_1, pf_d1_1),
-#          pf_d1_2 = ifelse(date > as.Date("2021-03-08"), pf_d1_2 + pf_d2_2, pf_d1_2),
-#          pf_d1_3 = ifelse(date > as.Date("2021-03-08"), pf_d1_3 + pf_d2_3, pf_d1_3),
-#          pf_d1_4 = ifelse(date > as.Date("2020-03-08"), pf_d1_4 + pf_d2_4, pf_d1_4),
-#          pf_d1_5 = ifelse(date > as.Date("2020-03-08"), pf_d1_5 + pf_d2_5, pf_d1_5),
-#          pf_d1_6 = ifelse(date > as.Date("2020-03-08"), pf_d1_6 + pf_d2_6, pf_d1_6),
-#          pf_d1_7 = ifelse(date > as.Date("2020-03-08"), pf_d1_7 + pf_d2_7, pf_d1_7),
-#          pf_d1_8 = ifelse(date > as.Date("2020-03-08"), pf_d1_8 + pf_d2_8, pf_d1_8),
-#          pf_d1_9 = ifelse(date > as.Date("2020-03-08"), pf_d1_9 + pf_d2_9, pf_d1_9),
-#          # pfizer dose 2 (make zero)
-#          pf_d2_1 = ifelse(date > as.Date("2021-03-08"), 0, pf_d2_1),
-#          pf_d2_2 = ifelse(date > as.Date("2021-03-08"), 0, pf_d2_2),
-#          pf_d2_3 = ifelse(date > as.Date("2021-03-08"), 0, pf_d2_3),
-#          pf_d2_4 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_4),
-#          pf_d2_5 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_5),
-#          pf_d2_6 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_6),
-#          pf_d2_7 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_7),
-#          pf_d2_8 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_8),
-#          pf_d2_9 = ifelse(date > as.Date("2020-03-08"), 0, pf_d2_9),
-#          # moderna dose 1
-#          mo_d1_1 = ifelse(date > as.Date("2021-03-08"), mo_d1_1 + mo_d2_1, mo_d1_1),
-#          mo_d1_2 = ifelse(date > as.Date("2021-03-08"), mo_d1_2 + mo_d2_2, mo_d1_2),
-#          mo_d1_3 = ifelse(date > as.Date("2021-03-08"), mo_d1_3 + mo_d2_3, mo_d1_3),
-#          mo_d1_4 = ifelse(date > as.Date("2020-03-08"), mo_d1_4 + mo_d2_4, mo_d1_4),
-#          mo_d1_5 = ifelse(date > as.Date("2020-03-08"), mo_d1_5 + mo_d2_5, mo_d1_5),
-#          mo_d1_6 = ifelse(date > as.Date("2020-03-08"), mo_d1_6 + mo_d2_6, mo_d1_6),
-#          mo_d1_7 = ifelse(date > as.Date("2020-03-08"), mo_d1_7 + mo_d2_7, mo_d1_7),
-#          mo_d1_8 = ifelse(date > as.Date("2020-03-08"), mo_d1_8 + mo_d2_8, mo_d1_8),
-#          mo_d1_9 = ifelse(date > as.Date("2020-03-08"), mo_d1_9 + mo_d2_9, mo_d1_9),
-#          # moderna dose 2 (make zero)
-#          mo_d2_1 = ifelse(date > as.Date("2021-03-08"), 0, mo_d2_1),
-#          mo_d2_2 = ifelse(date > as.Date("2021-03-08"), 0, mo_d2_2),
-#          mo_d2_3 = ifelse(date > as.Date("2021-03-08"), 0, mo_d2_3),
-#          mo_d2_4 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_4),
-#          mo_d2_5 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_5),
-#          mo_d2_6 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_6),
-#          mo_d2_7 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_7),
-#          mo_d2_8 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_8),
-#          mo_d2_9 = ifelse(date > as.Date("2020-03-08"), 0, mo_d2_9),
-#          # astrazeneca dose 1
-#          az_d1_1 = ifelse(date > as.Date("2021-03-08"), az_d1_1 + az_d2_1, az_d1_1),
-#          az_d1_2 = ifelse(date > as.Date("2021-03-08"), az_d1_2 + az_d2_2, az_d1_2),
-#          az_d1_3 = ifelse(date > as.Date("2021-03-08"), az_d1_3 + az_d2_3, az_d1_3),
-#          az_d1_4 = ifelse(date > as.Date("2020-03-08"), az_d1_4 + az_d2_4, az_d1_4),
-#          az_d1_5 = ifelse(date > as.Date("2020-03-08"), az_d1_5 + az_d2_5, az_d1_5),
-#          az_d1_6 = ifelse(date > as.Date("2020-03-08"), az_d1_6 + az_d2_6, az_d1_6),
-#          az_d1_7 = ifelse(date > as.Date("2020-03-08"), az_d1_7 + az_d2_7, az_d1_7),
-#          az_d1_8 = ifelse(date > as.Date("2020-03-08"), az_d1_8 + az_d2_8, az_d1_8),
-#          az_d1_9 = ifelse(date > as.Date("2020-03-08"), az_d1_9 + az_d2_9, az_d1_9),
-#          # astrazeneca dose 2 (make zero)
-#          az_d2_1 = ifelse(date > as.Date("2021-03-08"), 0, az_d2_1),
-#          az_d2_2 = ifelse(date > as.Date("2021-03-08"), 0, az_d2_2),
-#          az_d2_3 = ifelse(date > as.Date("2021-03-08"), 0, az_d2_3),
-#          az_d2_4 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_4),
-#          az_d2_5 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_5),
-#          az_d2_6 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_6),
-#          az_d2_7 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_7),
-#          az_d2_8 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_8),
-#          az_d2_9 = ifelse(date > as.Date("2020-03-08"), 0, az_d2_9)
-#          )
-# 
-# 
-# # for loop approach
-# dose2_stop_date <- as.Date("2021-03-08")
-# second_dose_scenario <- "none" # can also be "double" for doubling time in between
-# dates <- vac_schedule$date
-# vac_schedule2 <- vac_schedule[,-1] # we'll write over vac_schedule2
-# for (i in 1:dim(vac_schedule)[1]){
-#   if(dates[i] < dose2_stop_date){next}
-#   # check cumulative coverage so far
-#   cumulative_sum <- vac_schedule %>%
-#     filter(date <= dates[i]) %>%
-#     select(-date) %>%
-#     summarise_all(sum)
-#   
-#   pf_dose1 <- cumulative_sum %>% select(pf_d1_1:pf_d1_9)
-#   pf_dose2 <- cumulative_sum %>% select(pf_d2_1:pf_d2_9)
-#   mo_dose1 <- cumulative_sum %>% select(mo_d1_1:mo_d1_9)
-#   mo_dose2 <- cumulative_sum %>% select(mo_d2_1:mo_d2_9)
-#   az_dose1 <- cumulative_sum %>% select(az_d1_1:az_d1_9)
-#   az_dose2 <- cumulative_sum %>% select(az_d2_1:az_d2_9)
-#   
-#   dose1_all <- pf_dose1 + mo_dose1 + az_dose1
-#   # loop over age groups
-#   for (j in 1:9){
-#     # check if vac converage is < 1
-#     if(pf_dose1[j] < 1){
-#       # pfizer
-#       vac_schedule2[i,j] <- vac_schedule2[i,j] + vac_schedule2[i,j + 9]
-#       vac_schedule2[i,j + 9] <- 0
-#       # moderna
-#       if(pf_dose1[j] + mo_dose1[j])
-#       vac_schedule2[i,j + 18] <- vac_schedule2[i,j + 18] + vac_schedule2[i,j + 27]
-#       vac_schedule2[i,j + 27] <- 0
-#       #astrazeneca
-#       vac_schedule2[i,j + 36] <- vac_schedule2[i,j + 36] + vac_schedule2[i,j + 45]
-#       vac_schedule2[i,j + 45] <- 0
-#       
-#     } else if (dose1_all[j] >= 1){
-#       # pfizer
-#       new_perc_pf <- (vac_schedule2[i,j + 9] * n_vec[j])/n_vec[j-1]
-#       vac_schedule2[i,j-1] <- vac_schedule2[i,j-1] + new_perc_pf
-#       vac_schedule2[i,j + 9] <- 0
-#       # moderna
-#       new_perc_mo <- (vac_schedule2[i,j + 27] * n_vec[j])/n_vec[j-1]
-#       vac_schedule2[i,(j + 18) - 1] <- vac_schedule2[i,(j + 18) - 1] + new_perc_mo
-#       vac_schedule2[i,j + 27] <- 0
-#       #astrazeneca
-#       new_perc_az <- (vac_schedule2[i,j + 45] * n_vec[j])/n_vec[j-1]
-#       vac_schedule2[i,(j + 36) - 1] <- vac_schedule2[i,(j + 36) - 1] + new_perc_az
-#       vac_schedule2[i,j + 45] <- 0
-#     }
-#     
-#     
-#     
-#   } # end loop over age groups
-#   
-# } # end loop over dates
-# 
-# # check cumulative sums of vac_schedule2
-# cumulative_sum2 <- vac_schedule2 %>%
-#   summarise_all(sum)
 
 
 
