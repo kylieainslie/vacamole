cd03206b8d78a9f371cc2c065884d8010e427867
kylieainslie <ainslie.kylie@gmail.com>
2021-03-08 11:15:59 +0100

    look at total vaccine doses

--- inst/extdata/scripts/convert_vac_schedule.R
+++ inst/extdata/scripts/convert_vac_schedule.R
@@ -2,7 +2,8 @@
 library(readr)
 library(tidyr)
 library(dplyr)
-
+library(ggplot2)
+library(cowplot)
 # read in vac schedule file -----------------------------------------------------------------------
 cum_vac_schedule_orig <- read_csv("inst/extdata/data/Cum_upt_B_parallel_20210225.csv")
 cum_vac_schedule_no2dose<- read_csv("inst/extdata/data/Cum_upt_B_parallel_20210225 scno2nddose.csv")
@@ -14,8 +15,6 @@ age_dist_10 <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463,
               0.14514332, 0.12092904, 0.08807406, 0.03976755, 0.007398671)
 n <- 17407585 # Dutch population size
 n_vec_10 <- n * age_dist_10
-
-# calculate vac coverage (up to 1 July 2021)
 
 # take the difference for each row
 # original
@@ -87,15 +86,160 @@ vac_schedule_delay3mo_new <- bind_rows(vac_schedule_orig_8March, vac_schedule_de
   filter(date > as.Date("2021-01-31")) %>%
   add_row(before_feb, .before = 1)
 
+# calculate number of doses administered
+end_date <- as.Date("2021-07-01")
+# convert proportion vaccinated into number vaccinated
+num_vac_orig <- sweep(vac_schedule_orig_new[,-1], 2, n_vec, "*") %>%
+  mutate(date = as.Date(vac_schedule_orig_new$date, "%m/%d/%Y")) %>%
+  select(date, pf_d1_1:az_d2_9) %>%
+  filter(date <= end_date) %>%
+  summarise_at(vars(pf_d1_1:az_d2_9), sum)
+  
+num_vac_no2dose <- sweep(vac_schedule_no2dose_new[,-1], 2, n_vec, "*") %>%
+  mutate(date = as.Date(vac_schedule_no2dose_new$date, "%m/%d/%Y")) %>%
+  select(date, pf_d1_1:az_d2_9) %>%
+  filter(date <= end_date) %>%
+  summarise_at(vars(pf_d1_1:az_d2_9), sum)
+
+num_vac_delay3mo <- sweep(vac_schedule_delay3mo_new[,-1], 2, n_vec, "*") %>%
+  mutate(date = as.Date(vac_schedule_delay3mo_new$date, "%m/%d/%Y")) %>%
+  select(date, pf_d1_1:az_d2_9) %>%
+  filter(date <= end_date) %>%
+  summarise_at(vars(pf_d1_1:az_d2_9), sum)
+
+num_vac_all <- bind_rows(num_vac_orig,
+                         num_vac_delay3mo,
+                         num_vac_no2dose,
+                         .id = "scenario") %>%
+  mutate(scenario = case_when(
+    scenario == 1 ~ "original",
+    scenario == 2 ~ "delay 3 months",
+    scenario == 3 ~ "no 2nd doses"
+  )) %>%
+  # select(-date) %>%
+  pivot_longer(cols = pf_d1_1:az_d2_9,names_to = c("vaccine", "dose", "age_group"),
+               names_pattern = "(.*)_(.*)_(.*)", values_to = "n") %>%
+  mutate(vaccine = case_when(
+    vaccine == "pf" ~ "pfizer",
+    vaccine == "mo" ~ "moderna",
+    vaccine == "az" ~ "astrazeneca"),
+    dose = case_when(
+      dose == "d1" ~ 1,
+      dose == "d2" ~ 2),
+    dose = as.factor(dose),
+    age_group = case_when(
+      age_group == 1 ~ "0-9",
+      age_group == 2 ~ "10-19",
+      age_group == 3 ~ "20-29",
+      age_group == 4 ~ "30-39",
+      age_group == 5 ~ "40-49",
+      age_group == 6 ~ "50-59",
+      age_group == 7 ~ "60-69",
+      age_group == 8 ~ "70-79",
+      age_group == 9 ~ "80+"#,
+      #age_group == 10 ~ "90+"
+    )#,
+    #n_divide_100000 = n / 100000
+  )
+
+ymax = max(num_vac_all$n)
+
+p_orig <- ggplot(data = num_vac_all %>% filter(scenario == "original"), 
+                 aes(x = age_group, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax)) +
+  #geom_text(aes(label = n), position = position_dodge(width=0.9), vjust=-0.25) +
+  facet_grid(vars(vaccine), scales = "free") 
+
+p_delay3mo <- ggplot(data = num_vac_all %>% filter(scenario == "delay 3 months"), 
+                 aes(x = age_group, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax)) +
+  facet_grid(vars(vaccine), scales = "free")
+
+p_no2dose <- ggplot(data = num_vac_all %>% filter(scenario == "no 2nd doses"), 
+                 aes(x = age_group, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax)) +
+  facet_grid(vars(vaccine), scales = "free")
 
 
+# arrange the three plots in a single row
+prow <- plot_grid(
+  p_orig     + theme(legend.position="none"),
+  p_delay3mo + theme(legend.position="none"),
+  p_no2dose  + theme(legend.position="none"),
+  align = 'vh',
+  labels = c("A", "B", "C"),
+  hjust = -1,
+  nrow = 1
+)
+
+# extract the legend from one of the plots
+legend <- get_legend(
+  # create some space to the left of the legend
+  p_orig + theme(legend.box.margin = margin(0, 0, 0, 12))
+)
+
+# add the legend to the row we made earlier. Give it one-third of 
+# the width of one plot (via rel_widths).
+p <- plot_grid(prow, legend, rel_widths = c(3, .4))
+ggsave("inst/extdata/results/vac_allocation_by_age_group.jpg",
+       plot = p,
+       height = 6,
+       width = 15,
+       dpi = 300)
 
 
+# summarise over age groups
+totals <- num_vac_all %>%
+  group_by(scenario, vaccine, dose) %>%
+  summarise_at(.vars = "n", sum)
+#write_csv(totals, "")
 
+ymax2 <- max(totals$n)
 
+p_totals_orig <- ggplot(data = totals %>% filter(scenario == "original"), 
+                        aes(x = vaccine, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax2)) +
+  geom_text(aes(label = round(n,0)), position = position_dodge(width=0.9), vjust=-0.25)
+  # facet_grid(vars(vaccine), scales = "free")
 
+p_totals_delay3mo <- ggplot(data = totals %>% filter(scenario == "delay 3 months"), 
+                        aes(x = vaccine, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax2)) +
+  geom_text(aes(label = round(n,0)), position = position_dodge(width=0.9), vjust=-0.25)
 
+p_totals_no2dose <- ggplot(data = totals %>% filter(scenario == "no 2nd doses"), 
+                        aes(x = vaccine, y = n, fill = dose)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  scale_y_continuous(limits = c(0,ymax2)) +
+  geom_text(aes(label = round(n,0)), position = position_dodge(width=0.9), vjust=-0.25)
 
+# arrange the three plots in a single row
+prow2 <- plot_grid(
+  p_totals_orig     + theme(legend.position="none"),
+  p_totals_delay3mo + theme(legend.position="none"),
+  p_totals_no2dose  + theme(legend.position="none"),
+  align = 'vh',
+  labels = c("A", "B", "C"),
+  hjust = -1,
+  nrow = 1
+)
 
+# extract the legend from one of the plots
+legend2 <- get_legend(
+  # create some space to the left of the legend
+  p_totals_orig + theme(legend.box.margin = margin(0, 0, 0, 12))
+)
 
-
+# add the legend to the row we made earlier. Give it one-third of 
+# the width of one plot (via rel_widths).
+p2 <- plot_grid(prow2, legend2, rel_widths = c(3, .4))
+ggsave("inst/extdata/results/vac_allocation_totals.jpg",
+       plot = p2,
+       height = 6,
+       width = 15,
+       dpi = 300)
