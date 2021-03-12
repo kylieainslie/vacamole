4fd420b7477632e2ba52631d78dfa82e8b3ddd12
kylieainslie <ainslie.kylie@gmail.com>
2021-03-12 14:49:34 +0100

    update with new contact matrix selection criteria

--- R/get_foi.R
+++ R/get_foi.R
@@ -1,95 +1,66 @@
 #' Calculate force of infection 
 #' @param dat data frame of states
-#' @param beta transmission parameter
+#' @param params list of parameter values
 #' @param contact_matrix contact matrix
-#' @param N vector of total group sizes
 #' @return matrix of force of infection in each age group (columns) at each
 #' time point (rows)
 #' @keywords vacamole
 #' @export
-get_foi <- function(dat, 
-                    beta,
-                    sigma,
-                    i1,
-                    p_report,
-                    N,
-                    c_lockdown = NULL,
-                    c_relaxed = NULL,
-                    c_very_relaxed = NULL,
-                    c_normal = NULL,
-                    thresh_l,
-                    thresh_m,
-                    thresh_u,
-                    use_cases = TRUE,
-                    force_relax = NULL
-                    ){
+get_foi <- function(dat, params){
 
+    # define objects from params
+    
     # sum over different I states for each time step and age group
+    # S <- as.matrix(dat$S)
+    # Shold_1d <- as.matrix(dat$Shold_1d)
+    # Sv_1d <- as.matrix(dat$Sv_1d)
+    # Shold_2d <- as.matrix(dat$Shold_2d)
+    # Sv_2d <- as.matrix(dat$Sv_2d)
+    E_all <- as.matrix(dat$E + dat$Ev_1d + dat$Ev_2d)
     I_all <- as.matrix(dat$I + dat$Iv_1d + dat$Iv_2d)
     H_all <- as.matrix(dat$H + dat$Hv_1d + dat$Hv_2d)
-    E_all <- as.matrix(dat$E + dat$Ev_1d + dat$Ev_2d)
+    
     
     # calculate force of infection for each time point
-    #foi <- t(apply(I_all, 1, function(x){beta * (c_main %*% (x/N))}))
     time_vec <- 1:dim(I_all)[1]
     ic_admin <- rep(0, length(time_vec))
     cases    <- rep(0, length(time_vec))
     criteria <- rep(0, length(time_vec))
-    slope    <- rep(0, length(time_vec))
-    cm_check <- rep(NA, length(time_vec))
+    #slope    <- rep(0, length(time_vec))
+    #cm_check <- rep(NA, length(time_vec))
     
     for(t in time_vec){
       ic_admin[t] <- sum(i1 * H_all[t,])
       cases[t] <- sum(sigma * E_all[t,] * p_report)
       
       criteria[t] <- (use_cases) * cases[t] + (!use_cases) * ic_admin[t] 
-      slope[t] <- ifelse( t == 1, 0, criteria[t] - criteria[t-1])
       
-      contact_mat <- (is.null(force_relax)) *
-        ((criteria[t] < thresh_u & criteria[t] >= thresh_m & slope[t] < 0) * c_lockdown +
-           (criteria[t] < thresh_m & criteria[t] >= thresh_l & slope[t] < 0) * c_relaxed +
-           (criteria[t] < thresh_l) * c_very_relaxed +
-           (criteria[t] >= thresh_l & criteria[t] <= thresh_m & slope[t] > 0) * c_very_relaxed +
-           (criteria[t] > thresh_m & criteria[t] <= thresh_u & slope[t] > 0) * c_relaxed +
-           (criteria[t] > thresh_u) * c_lockdown +
-           (criteria[t] >= 0 & criteria[t] < thresh_l & slope[t] > 0) * c_normal) +
-        (!is.null(force_relax) & (t < force_relax)) *
-        ((criteria[t] < thresh_u & criteria[t] >= thresh_m & slope[t] < 0) * c_lockdown +
-           (criteria[t] < thresh_m & criteria[t] >= thresh_l & slope[t] < 0) * c_relaxed +
-           (criteria[t] < thresh_l) * c_very_relaxed +
-           (criteria[t] >= thresh_l & criteria[t] <= thresh_m & slope[t] > 0) * c_very_relaxed +
-           (criteria[t] > thresh_m & criteria[t] <= thresh_u & slope[t] > 0) * c_relaxed +
-           (criteria[t] > thresh_u) * c_lockdown +
-           (criteria[t] >= 0 & criteria[t] < thresh_l & slope[t] > 0) * c_normal) +
-        (!is.null(force_relax) & (t >= force_relax)) *
-        ((criteria[t] < thresh_u & criteria[t] >= thresh_m & slope[t] < 0) * c_relaxed +
-           (criteria[t] < thresh_m & criteria[t] >= thresh_l & slope[t] < 0) * c_relaxed +
-           (criteria[t] < thresh_l) * c_very_relaxed +
-           (criteria[t] >= thresh_l & criteria[t] <= thresh_m & slope[t] > 0) * c_very_relaxed +
-           (criteria[t] > thresh_m & criteria[t] <= thresh_u & slope[t] > 0) * c_relaxed +
-           (criteria[t] > thresh_u) * c_relaxed +
-           (criteria[t] >= 0 & criteria[t] < thresh_l & slope[t] > 0) * c_normal)
+      #if(t == 0 ){cases <- sum(init_lambda * ((S[t,] + Shold_1d[t,]) + eta * (Sv_1d[t,] + Shold_2d[t,]) + eta2 * Sv_2d[t,]))}
+      #criteria <- (use_cases) * cases + (!use_cases) * ic_admin 
       
-      # contact_mat <- (criteria[t] < thresh_u & criteria[t] >= thresh_m & slope[t] < 0) * c_lockdown +
-      #   (criteria[t] < thresh_m & criteria[t] >= thresh_l & slope[t] < 0) * c_relaxed +
-      #   (criteria[t] < thresh_l) * c_very_relaxed +
-      #   (criteria[t] >= thresh_l & criteria[t] <= thresh_m & slope[t] > 0) * c_very_relaxed +
-      #   (criteria[t] > thresh_m & criteria[t] <= thresh_u & slope[t] > 0) * c_relaxed +
-      #   (criteria[t] > thresh_u) * c_lockdown +
-      #   (criteria[t] >= 0 & criteria[t] < thresh_l & slope[t] > 0) * c_normal
+      if(t == 0){
+        flag_relaxed <- 0
+        flag_very_relaxed <- 0
+        flag_normal <- 0
+      }
       
-      # contact_mat <- (t < 20) * c_lockdown +
-      #   (criteria[t] < thresh_u & criteria[t] >= thresh_l & slope[t] < 0 & t >=20) * c_lockdown +
-      #   (criteria[t] < thresh_l & t >=20) * c_relaxed +
-      #   (criteria[t] >= thresh_l & criteria[t] <= thresh_u & slope[t] > 0 & t >=20) * c_relaxed +
-      #   (criteria[t] > thresh_u & t >=20) * c_lockdown 
+      tmp2 <- choose_contact_matrix(params, times, criteria, flag_relaxed, 
+                                    flag_very_relaxed, flag_normal)
+      contact_mat <- tmp2$contact_matrix
+      flag_relaxed <- tmp2$flag_relaxed
+      flag_very_relaxed <- tmp2$flag_very_relaxed
+      flag_normal <- tmp2$flag_normal
       
+      # determine force of infection ----------------------------------
+      lambda <- beta * delta * (contact_mat %*% (I + Iv_1d + Iv_2d))
+      # ---------------------------------------------------------------
+      
+      # check for which contact metrix was selected -------------------
       cm_check[t] <- ifelse(identical(contact_mat, c_lockdown), "c_lockdown", 
                             ifelse(identical(contact_mat, c_relaxed),"c_relaxed",
                                    ifelse(identical(contact_mat, c_very_relaxed),"c_very_relaxed", "c_normal")))
-
-      lambda <- beta * (contact_mat %*% I_all[t,])
-
+      # ---------------------------------------------------------------
+  
       if (t == 1){ rtn <- data.frame(time = t-1, age_group = 1:9, foi = lambda)
       } else { 
         tmp <- data.frame(time = t-1, age_group = 1:9, foi = lambda)
@@ -98,7 +69,7 @@ get_foi <- function(dat,
   
     rtn2 <- list(check = data.frame(time = time_vec - 1, 
                                 criteria = criteria,
-                                slope = slope,
+                                #slope = slope,
                                 cm_check = cm_check),
                  lambda = rtn)
   
