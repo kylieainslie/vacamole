9eb1afb03e11ba0227f056c26bc74fef4ef454b5
kylieainslie <ainslie.kylie@gmail.com>
2021-03-30 14:05:36 +0200

    turn convert_vsc_schedule script into a function

--- inst/extdata/scripts/convert_vac_schedule.R
+++ inst/extdata/scripts/convert_vac_schedule.R
@@ -5,8 +5,9 @@ library(dplyr)
 library(ggplot2)
 library(cowplot)
 # read in vac schedule file -----------------------------------------------------------------------
-cum_vac_sched <- read_csv("inst/extdata/data/Cum_upt_B_parallel_20210311 Basis scenario.csv")
+cum_vac_sched <- read_csv("inst/extdata/data/Cum_upt_B_parallel_20210329 Basis.csv")
 
+convert_vac_schedule <- function(vac_schedule){
 # to combine age groups 9 and 10
 age_dist_10 <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463, 
               0.14514332, 0.12092904, 0.08807406, 0.03976755, 0.007398671)
@@ -43,34 +44,37 @@ vac_schedule_orig_new <- vac_schedule_orig %>%
   filter(date > as.Date("2021-01-31")) %>%
   add_row(before_feb, .before = 1)
 
+return(vac_schedule_orig_new)
+}
+
 # convert to number of vaccines
-age_dist_9 <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463, 
-              0.14514332, 0.12092904, 0.08807406, 0.04622194)
-n_vec_9 <- n * age_dist_9
-
-num_vacs <- sweep(vac_schedule_orig_new[,-1], 2, n_vec_9, "*") %>%
-  mutate(date = vac_schedule_orig_new$date) %>%
-  select(date, pf_d1_1:Ja_d2_9)
-
-# read in num vacs and then convert back to proportion
-num_o2y <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_o2y.csv")
-num_y2o <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_y2o.csv")
-num_alt <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_alt.csv")
-num_no_vac_healthy <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_no_vac_healthy.csv")
-
-prop_o2y <- sweep(num_o2y[,-1], 2, n_vec_9, "/") %>%
-  mutate(date = vac_schedule_orig_new$date) %>%
-  select(date, pf_d1_1:Ja_d2_9)
-
-prop_y2o <- sweep(num_y2o[,-1], 2, n_vec_9, "/") %>%
-  mutate(date = vac_schedule_orig_new$date) %>%
-  select(date, pf_d1_1:Ja_d2_9)
-
-prop_alt <- sweep(num_alt[,-1], 2, n_vec_9, "/") %>%
-  mutate(date = vac_schedule_orig_new$date) %>%
-  select(date, pf_d1_1:Ja_d2_9)
-
-prop_no_vac_healthy <- sweep(num_no_vac_healthy[,-1], 2, n_vec_9, "/") %>%
-  mutate(date = vac_schedule_orig_new$date) %>%
-  select(date, pf_d1_1:Ja_d2_9)
+# age_dist_9 <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463, 
+#               0.14514332, 0.12092904, 0.08807406, 0.04622194)
+# n_vec_9 <- n * age_dist_9
+# 
+# num_vacs <- sweep(vac_schedule_orig_new[,-1], 2, n_vec_9, "*") %>%
+#   mutate(date = vac_schedule_orig_new$date) %>%
+#   select(date, pf_d1_1:Ja_d2_9)
+# 
+# # read in num vacs and then convert back to proportion
+# num_o2y <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_o2y.csv")
+# num_y2o <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_y2o.csv")
+# num_alt <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_alt.csv")
+# num_no_vac_healthy <- read_csv("inst/extdata/data/num_vac_basis_scenario_20210311_no_vac_healthy.csv")
+# 
+# prop_o2y <- sweep(num_o2y[,-1], 2, n_vec_9, "/") %>%
+#   mutate(date = vac_schedule_orig_new$date) %>%
+#   select(date, pf_d1_1:Ja_d2_9)
+# 
+# prop_y2o <- sweep(num_y2o[,-1], 2, n_vec_9, "/") %>%
+#   mutate(date = vac_schedule_orig_new$date) %>%
+#   select(date, pf_d1_1:Ja_d2_9)
+# 
+# prop_alt <- sweep(num_alt[,-1], 2, n_vec_9, "/") %>%
+#   mutate(date = vac_schedule_orig_new$date) %>%
+#   select(date, pf_d1_1:Ja_d2_9)
+# 
+# prop_no_vac_healthy <- sweep(num_no_vac_healthy[,-1], 2, n_vec_9, "/") %>%
+#   mutate(date = vac_schedule_orig_new$date) %>%
+#   select(date, pf_d1_1:Ja_d2_9)
 
