9ad5b9f80e379a724f9b7c1f4b29c53b1b1300e4
kylieainslie <ainslie.kylie@gmail.com>
2021-04-13 11:40:56 +0200

    move get_vac_rate code outside main simulation

--- inst/extdata/scripts/convert_vac_schedule.R
+++ inst/extdata/scripts/convert_vac_schedule.R
@@ -44,6 +44,109 @@ vac_schedule_orig_new <- vac_schedule_orig %>%
   filter(date > as.Date("2021-01-31")) %>%
   add_row(before_feb, .before = 1)
 
+vac_schedule_new_cs <- cumsum(vac_schedule_orig_new[,-1])
+
+# pfizer
+pf_dose1 <- vac_schedule_orig_new %>% select(date, pf_d1_1:pf_d1_9)
+pf_dose1_cs <- vac_schedule_new_cs %>% select(pf_d1_1:pf_d1_9)
+pf_dose2 <- vac_schedule_orig_new %>% select(date, pf_d2_1:pf_d2_9)
+pf_dose2_cs <- vac_schedule_new_cs %>% select(pf_d2_1:pf_d2_9)
+
+# moderna
+mo_dose1 <- vac_schedule_orig_new %>% select(date, mo_d1_1:mo_d1_9)
+mo_dose1_cs <- vac_schedule_new_cs %>% select(mo_d1_1:mo_d1_9)
+mo_dose2 <- vac_schedule_orig_new %>% select(date, mo_d2_1:mo_d2_9)
+mo_dose2_cs <- vac_schedule_new_cs %>% select(mo_d2_1:mo_d2_9)
+
+# astrazeneca
+az_dose1 <- vac_schedule_orig_new %>% select(date, az_d1_1:az_d1_9)
+az_dose1_cs <- vac_schedule_new_cs %>% select(az_d1_1:az_d1_9)
+az_dose2 <- vac_schedule_orig_new %>% select(date, az_d2_1:az_d2_9)
+az_dose2_cs <- vac_schedule_new_cs %>% select(az_d2_1:az_d2_9)
+
+# jansen
+ja_dose1 <- vac_schedule_orig_new %>% select(date, Ja_d1_1:Ja_d1_9)
+ja_dose1_cs <- vac_schedule_new_cs %>% select(Ja_d1_1:Ja_d1_9)
+ja_dose2 <- vac_schedule_orig_new %>% select(date, Ja_d2_1:Ja_d2_9)
+ja_dose2_cs <- vac_schedule_new_cs %>% select(Ja_d2_1:Ja_d2_9)
+
+
+# calculate composite VE
+name_suffix_d1 <- c(substr(names(pf_dose1[,-1]), 3, 7))
+name_suffix_d2 <- c(substr(names(pf_dose2[,-1]), 3, 7))
+
+alpha_dose1 <- pf_dose1[,-1] + mo_dose1[,-1] + az_dose1[,-1] + ja_dose1[,-1]
+names(alpha_dose1) <- paste0("alpha", name_suffix_d1)
+alpha_dose2 <- pf_dose2[,-1] + mo_dose2[,-1] + az_dose2[,-1] + ja_dose2[,-1]
+names(alpha_dose2) <- paste0("alpha", name_suffix_d2)
+
+total_dose1 <- pf_dose1_cs + mo_dose1_cs + az_dose1_cs + ja_dose1_cs
+names(total_dose1) <- paste0("tot", name_suffix_d1)
+total_dose2 <- pf_dose2_cs + mo_dose2_cs + az_dose2_cs + ja_dose2_cs
+names(total_dose2) <- paste0("tot", name_suffix_d2)
+
+frac_pf_dose1 <- pf_dose1_cs/total_dose1
+frac_pf_dose1 <- ifelse(is.nan(frac_pf_dose1), 0, frac_pf_dose1)
+frac_pf_dose2 <- pf_dose2_cs/total_dose2
+#frac_pf_dose2 <- ifelse(is.nan(frac_pf_dose2), 0, frac_pf_dose2)
+
+frac_mo_dose1 <- mo_dose1_cs/total_dose1
+#frac_mo_dose1 <- ifelse(is.nan(frac_mo_dose1), 0, frac_mo_dose1)
+frac_mo_dose2 <- mo_dose2_cs/total_dose2
+#frac_mo_dose2 <- ifelse(is.nan(frac_mo_dose2), 0, frac_mo_dose2)
+
+frac_az_dose1 <- az_dose1_cs/total_dose1
+#frac_az_dose1 <- ifelse(is.nan(frac_az_dose1), 0, frac_az_dose1)
+frac_az_dose2 <- az_dose2_cs/total_dose2
+#frac_az_dose2 <- ifelse(is.nan(frac_az_dose2), 0, frac_az_dose2)
+
+frac_ja_dose1 <- ja_dose1_cs/total_dose1
+#frac_ja_dose1 <- ifelse(is.nan(frac_ja_dose1), 0, frac_az_dose1)
+frac_ja_dose2 <- ja_dose2_cs/total_dose2
+#frac_ja_dose2 <- ifelse(is.nan(frac_ja_dose2), 0, frac_ja_dose2)
+
+comp_ve_dose1 <- frac_pf_dose1 * ve$pfizer[1] + 
+  frac_mo_dose1 * ve$moderna[1] + 
+  frac_az_dose1 * ve$astrazeneca[1] +
+  frac_ja_dose1 * ve$jansen
+comp_ve_dose2 <- frac_pf_dose2 * ve$pfizer[2] + 
+  frac_mo_dose2 * ve$moderna[2] + 
+  frac_az_dose2 * ve$astrazeneca[2] +
+  frac_ja_dose2 * ve$jansen
+
+# rate of hospitalisations multiplier
+hosp_mult_dose1 <- frac_pf_dose1 * hosp_multiplier$pfizer[1] +
+  frac_mo_dose1 * hosp_multiplier$moderna[1] +
+  frac_az_dose1 * hosp_multiplier$astrazeneca[1] +
+  frac_ja_dose1 * hosp_multiplier$jansen
+hosp_mult_dose2 <- frac_pf_dose2 * hosp_multiplier$pfizer[2] +
+  frac_mo_dose2 * hosp_multiplier$moderna[2] +
+  frac_az_dose2 * hosp_multiplier$astrazeneca[2] +
+  frac_ja_dose2 * hosp_multiplier$jansen
+
+# composite delay to protection
+delay_dose1 <- frac_pf_dose1 * delay$pfizer[1] +
+  frac_mo_dose1 * delay$moderna[1] +
+  frac_az_dose1 * delay$astrazeneca[1] +
+  frac_ja_dose1 * delay$jansen
+delay_dose1 <- ifelse(delay_dose1 == 0, 1, delay_dose1) # this prevents from dividing by 0 in the ODEs
+delay_dose2 <- frac_pf_dose2 * delay$pfizer[2] +
+  frac_mo_dose2 * delay$moderna[1] +
+  frac_az_dose2 * delay$astrazeneca[2] +
+  frac_ja_dose2 * delay$jansen
+delay_dose2 <- ifelse(delay_dose2 == 0, 1, delay_dose2) # this prevents from dividing by 0 in the ODEs
+
+eta_vec <- 1 - ifelse(is.nan(comp_ve_dose1), 0, comp_ve_dose1)
+names(eta_vec) <- paste0("eta_",1:9)
+eta2_vec <- 1- ifelse(is.nan(comp_ve_dose2), 0, comp_ve_dose2)
+names(eta2_vec) <- paste0("eta2_",1:9)
+
+comp_h_mult_dose1 <- 1 - ifelse(is.nan(hosp_mult_dose1), 0, hosp_mult_dose1)
+names(comp_h_mult_dose1) <- paste0("hosp_mult_",1:9)
+comp_h_mult_dose2 <- 1- ifelse(is.nan(hosp_mult_dose2), 0, hosp_mult_dose2)
+names(comp_h_mult_dose2) <- paste0("hosp_mult2_",1:9)
+
+
 return(vac_schedule_orig_new)
 }
 
