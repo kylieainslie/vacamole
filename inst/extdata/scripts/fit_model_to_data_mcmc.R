4ead1ec4a2e5bc79003197ea99a6bc9d724b9f73
kylieainslie <ainslie.kylie@gmail.com>
2021-04-16 12:49:22 -0400

    fix mis-match of daily real data and weekly modelled data

--- inst/extdata/scripts/fit_model_to_data_mcmc.R
+++ inst/extdata/scripts/fit_model_to_data_mcmc.R
@@ -19,7 +19,7 @@ parTab <- data.frame(names=c("beta"),
                      upper_bound=c(1))
 
 mcmcPars <- c("iterations"=2000,"popt"=0.44,"opt_freq"=100,
-              "thin"=1,"adaptive_period"=1000,"save_block"=5)
+              "thin"=1,"adaptive_period"=1000,"save_block"=100)
 
 
 ## Putting model solving code in a function for later use
@@ -55,9 +55,9 @@ create_lik <- function(parTab, data, PRIOR_FUNC,...){
     names(pars) <- par_names
     incidence <- solve_model(pars, t, params, age_struct_seir_ode)
     ## Get weekly incidence
-    weekly_incidence <- colSums(matrix(incidence,nrow=7/tstep))
+    #weekly_incidence <- colSums(matrix(incidence,nrow=7/tstep))
     
-    lik <- sum(dpois(x=inc,lambda=weekly_incidence,log=TRUE))
+    lik <- sum(dpois(x = inc,lambda = incidence,log=TRUE))
     if(!is.null(PRIOR_FUNC)) lik <- lik + PRIOR_FUNC(pars)
     lik
     
@@ -72,66 +72,72 @@ startTab$values <- c(0.0006)
 tstep <- 1
 weeks <- floor(dim(osiris1)[1]/7)
 t <- seq(0,(weeks*7)-1,by=tstep)
+osiris2 <- osiris1[t+1,]
 
-output <- run_MCMC(parTab=startTab, data=osiris1[t+1,], mcmcPars=mcmcPars, filename="SEIR_fitting",
+output <- run_MCMC(parTab=startTab, data=osiris2, mcmcPars=mcmcPars, filename="SEIR_fit_no_prior",
                    CREATE_POSTERIOR_FUNC = create_lik, mvrPars = NULL, PRIOR_FUNC=NULL,
                    params = params, age_struct_seir_ode = age_struct_seir_ode)
-chain <- read.csv(output$file)
+#chain <- read.csv(output$file)
+chain <- read.csv("SEIR_fit_no_prior_univariate_chain.csv")
 plot(coda::as.mcmc(chain[,c("beta")]))
 
+chain1 <- chain[chain$sampno >= 200, ] # mcmcPars["adaptive_period"] ,2:(ncol(chain)-1)
+
 ## Prior function
-my_prior <- function(pars){
-  ## Diffuse gaussian priors on both parameters
-  beta_prior <- dgamma(pars[1],0.5,0)
-  #gamma_prior <- dnorm(pars[2],0.2,100,1)
-  return(beta_prior)
-}
+# my_prior <- function(pars){
+#   ## Diffuse gaussian priors on both parameters
+#   beta_prior <- dgamma(pars[1],0.001,1000)
+#   #gamma_prior <- dnorm(pars[2],0.2,100,1)
+#   return(beta_prior)
+# }
 
 ## Use the previous chain to get a good starting
 ## covariance matrix
-startTab$values <- get_best_pars(chain)
-chain <- chain[chain$sampno >= mcmcPars["adaptive_period"],2:(ncol(chain)-1)]
-covMat <- cov(chain)
-mvrPars <- list(covMat,2.38/sqrt(nrow(parTab[parTab$fixed==0,])),w=0.8)
-
-## Remove restrictions on parameter bounds -
-## not important with the multivariate sampler
-startTab$lower_bound <- -Inf
-startTab$upper_bound <- Inf
-
-## Run with mvrPars
-mcmcPars <- c("iterations"=20000,"popt"=0.234,"opt_freq"=500,
-              "thin"=1,"adaptive_period"=7500,"save_block"=100)
-
-output2 <- run_MCMC(parTab=startTab, data=dat, mcmcPars=mcmcPars, filename="SIR_fitting",
-                    CREATE_POSTERIOR_FUNC = create_lik, mvrPars = mvrPars, PRIOR_FUNC=my_prior,
-                    S0=999,I0=1,R0=0,SIR_odes=SIR_odes)
-chain2 <- read.csv(output2$file)
-chain2 <- chain2[chain2$sampno >= mcmcPars["adaptive_period"],]
-plot(coda::as.mcmc(chain2[,c("R0","gamma")]))
+# startTab$values <- get_best_pars(chain)
+# chain <- chain[chain$sampno >= mcmcPars["adaptive_period"],2:(ncol(chain)-1)]
+# covMat <- cov(chain)
+# mvrPars <- list(covMat,2.38/sqrt(nrow(parTab[parTab$fixed==0,])),w=0.8)
+# 
+# ## Remove restrictions on parameter bounds -
+# ## not important with the multivariate sampler
+# startTab$lower_bound <- -Inf
+# startTab$upper_bound <- Inf
+# 
+# ## Run with mvrPars
+# mcmcPars <- c("iterations"=20000,"popt"=0.234,"opt_freq"=500,
+#               "thin"=1,"adaptive_period"=7500,"save_block"=100)
+# 
+# output2 <- run_MCMC(parTab=startTab, data=dat, mcmcPars=mcmcPars, filename="SIR_fitting",
+#                     CREATE_POSTERIOR_FUNC = create_lik, mvrPars = mvrPars, PRIOR_FUNC=my_prior,
+#                     S0=999,I0=1,R0=0,SIR_odes=SIR_odes)
+# chain2 <- read.csv(output2$file)
+# chain2 <- chain2[chain2$sampno >= mcmcPars["adaptive_period"],]
+# plot(coda::as.mcmc(chain2[,c("R0","gamma")]))
 
 ## Get the maximum likelihood parameters and estimate
 ## the model trajectory for each week
-best_pars <- get_best_pars(chain2)
-times <- seq(1,52*7,by=1)
-best_trajectory <- solve_model(best_pars,times,S0, I0, R0, SIR_odes)
+best_pars <- get_best_pars(chain1)
+times <- seq(1,weeks*7,by=1)
+best_trajectory <- solve_model(best_pars, times, params, age_struct_seir_ode)
 best_incidence <- colSums(matrix(best_trajectory,nrow=7))
-best_dat <- data.frame(t=seq(1,52,by=1), y=best_incidence)
+best_dat <- data.frame(t=seq(1,weeks,by=1), y=best_incidence)
 
 ## Bit of code to generate prediction intervals
-n_samps <- 100
-trajectories <- matrix(nrow=n_samps,ncol=52)
-samps <- sample(nrow(chain2),n_samps)
+n_samps <- 100 #100
+trajectories <- matrix(nrow=n_samps,ncol=weeks)
+samps <- sample(nrow(chain1),n_samps)
 for(i in 1:n_samps){
-  pars <- as.numeric(chain2[samps[i],c("R0","gamma")])
-  names(pars) <- c("R0","gamma")
-  tmp <- solve_model(pars, times, S0, I0, R0, SIR_odes)
+  print(i)
+  pars <- as.numeric(chain1[samps[i],c("beta")])
+  names(pars) <- c("beta")
+  tmp <- solve_model(pars, t, params, age_struct_seir_ode)
   trajectories[i,] <- colSums(matrix(tmp, nrow=7))
 }
 bounds <- apply(trajectories,2,function(x) quantile(x, c(0.025,0.975)))
 
-plot(dat$inc~seq(1,52,by=1),col="red",pch=16, 
-     xlab="Time (weeks)",ylab="Incidence",ylim=c(0,100))
+osiris_weekly_inc <- colSums(matrix(osiris2$inc,nrow=7))
+plot(osiris_weekly_inc~seq(1,weeks,by=1),col="red",pch=16, 
+     xlab="Time (weeks)",ylab="Incidence", ylim=c(0,42000)) 
 lines(best_dat,col="blue")
 lines(bounds[1,],col="blue",lty=2,lwd=0.5)
 lines(bounds[2,],col="blue",lty=2,lwd=0.5)
