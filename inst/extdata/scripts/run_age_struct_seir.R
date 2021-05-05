56063e3dede3a061d70a8bcafacded7b705c08b9
kylieainslie <ainslie.kylie@gmail.com>
2021-05-05 12:18:51 -0400

    time varying beta preamble

--- inst/extdata/scripts/run_age_struct_seir.R
+++ inst/extdata/scripts/run_age_struct_seir.R
@@ -2,11 +2,23 @@
 
 # Data and model parameters are loaded/defined in the script inst/extdata/scripts/model_run_helper.R
 
+# Determine time points over which to solve model
+start_date <- yday(as.Date("2021-01-31"))
+breakpoints <- yday(c(as.Date("2021-03-07"), 
+                      as.Date("2021-03-21"),
+                      as.Date("2021-04-01")))
+end_date <- lubridate::yday(as.Date("2021-12-31"))
+times_forward <- seq(0, end_date - start_date, by = 1)
+
+# define time varying parameters
+deltas <- c(1, 3.5, 0.5, 1.4)
+beta_mle <- 0.0004
+
 # Create list of parameter values for input into model solver
-params <- list(beta = beta2_prime,           # transmission rate
+params <- list(beta = beta_mle,           # transmission rate
                gamma = g,                      # 1/gamma = infectious period
                sigma = s,                      # 1/sigma = latent period
-               delta = 1,                      # scaling constant for beta
+               delta = NULL,                 # scaling constant for beta (if NULL it is excluded)
                N = n_vec,                      # Population (no need to change)
                h = h,                          # Rate from infection to hospital admission/ time from infection to hosp admission
                i1 = i1,
@@ -22,36 +34,29 @@ params <- list(beta = beta2_prime,           # transmission rate
                c_relaxed = t4,
                c_very_relaxed = t3,
                c_normal = t1,
+               keep_cm_fixed = TRUE,
                vac_inputs = basis1,
-               use_cases = TRUE,              # use cases as criteria to change contact matrices. If FALSE, IC admissions used.
+               use_cases = TRUE,                           # use cases as criteria to change contact matrices. If FALSE, IC admissions used.
                thresh_n = 0.5/100000 * sum(n_vec),
                thresh_l = 5/100000 * sum(n_vec),           # 3 for IC admissions
                thresh_m = 14.3/100000 * sum(n_vec),        # 10 for IC admissions
                thresh_u = 35.7/100000 * sum(n_vec),        # 20 for IC admissions
                no_vac = FALSE,
-               t_calendar_start = yday(as.Date("2021-01-31")),   # calendar start date (ex: if model starts on 31 Jan, then t_calendar_start = 31)
-               breakpoints = c() # time points when parameters can change (if NULL, then beta is constant over time)
-)
+               t_calendar_start = start_date,              # calendar start date (ex: if model starts on 31 Jan, then t_calendar_start = 31)
+               breakpoints = NULL  # breakpoints - start_date    # time points when parameters can change (if NULL, then beta is constant over time)
+               )
 
-# Determine time points over which to solve model
-t_max <- lubridate::yday(as.Date("2021-04-20")) # last day of osiris data that we fit
-times_fit <- seq(0,t_max - params$t_calendar_start, by = 1)  
-
-
-end_date <- lubridate::yday(as.Date("2021-08-31"))
-times_forward <- seq(0, end_date - params$t_calandar_start, by = 1)
 # Solve model ------------------------------------------------------
-seir_out <- lsoda(init,times_forward,age_struct_seir_ode,params) #
+seir_out <- lsoda(init,times,age_struct_seir_ode,params) #
 seir_out <- as.data.frame(seir_out)
 out <- postprocess_age_struct_model_output(seir_out)
 
 # quick check ------------------------------------------------------
-cases_from_fit <- (params$sigma * (init_from_fit1$E + init_from_fit1$Ev_1d + init_from_fit1$Ev_2d)) / 3
-cases <- (params$sigma * (out$E + out$Ev_1d + out$Ev_2d)) / 3
-cases_all <- bind_rows(cases_from_fit, cases)
-plot(seq(1, dim(cases_all)[1], by = 1), rowSums(cases_all), type = "l", col = "blue",
+# cases_from_fit <- (params$sigma * (init_from_fit1$E + init_from_fit1$Ev_1d + init_from_fit1$Ev_2d)) / 3
+cases <- (params$sigma * (out$E + out$Ev_1d + out$Ev_2d)) * params$p_report
+plot(seq(1, dim(cases)[1], by = 1), rowSums(cases), type = "l", col = "blue",
      xlab="Time (days)",ylab="Daily Cases", ylim = c(0,7000))
-points(osiris2$inc~seq(1,weeks*7,by=1),col="red",pch=16) 
+points(osiris2$inc~times,col="red",pch=16) 
 
 
 # Summarise results ------------------------------------------------
