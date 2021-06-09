151b789d8cfc024f2c3a8f6cd6d8119f9720f54a
Kylie Ainslie <kylieainslie@Kylies-MacBook-Pro.local>
2021-06-09 11:56:49 +0200

    fix type in lambda calculation

--- R/get_foi.R
+++ R/get_foi.R
@@ -43,7 +43,7 @@ get_foi <- function(dat, params, vac_inputs){
       
       # lambda <- params$beta * (contact_mat %*% (I_all[t,]))
       beta_t <- params$beta * (1 + params$beta1 * cos(2 * pi * calendar_day/365.24))
-      lambda <- beta_t * (contact_mat %*% (I + (eta_trans * Iv_1d) + (eta_trans2 * Iv_2d)))
+      lambda <- beta_t * (contact_mat %*% I_all[t,])
       
       # ---------------------------------------------------------------
       
