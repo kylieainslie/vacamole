a6242a853fe805c6b9eb8481bdbee56f8fe0e560
Kylie Ainslie <kylie.ainslie@rivm.nl>
2021-07-07 13:36:33 +0200

    my rmultinomial function to call within apply()

---
+++ R/my_rmultinom.R
@@ -0,0 +1,14 @@
+my_rmultinom <- function(x){
+  
+  size <- x[1]
+  p1 <- x[2]
+  p2 <- x[3]
+  
+  if(length(x) == 4){
+    p3 <- x[4]
+    prob_vec <- c(p1, p2, p3)
+  } else {prob_vec <- c(p1, p2)}
+  
+  rmultinom(n = 1, size = size, prob = prob_vec)
+}
+
