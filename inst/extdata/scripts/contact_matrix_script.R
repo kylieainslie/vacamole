128776d9f58ec03d0e5abb9ce43a594ddb2ac31a
Kylie Ainslie <kylie.ainslie@rivm.nl>
2021-07-29 16:12:28 +0200

    create function to convert contact matrices to transmission matrices

--- inst/extdata/scripts/contact_matrix_script.R
+++ inst/extdata/scripts/contact_matrix_script.R
@@ -2,13 +2,16 @@
 library(plyr)
 library(dplyr)
 library(tidyr)
-# read in all contact matrices
-#contact_matrices_pico3 <- read.delim("inst/extdata/data/S2_contact_matrices_withPico3_10y.tsv", header=TRUE, allowEscapes=FALSE, sep="\t")
-contact_matrices_pico4 <- readRDS("inst/extdata/data/Contactpatterns_PICO4_10y.rds") # february 2021
-contact_matrices_pico5 <- readRDS("inst/extdata/data/Contactpatterns_PICO5_10y.rds") # june 2021
 
-var_names <- paste0("c_smt.", 1:200)
+# read in all contact matrices ------------------------------------
+contact_matrices_pienter3 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_Pienter3_10y.rds") # september 2020
+contact_matrices_pico1 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_PICO1_10y.rds") # september 2020
+contact_matrices_pico2 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_PICO2_10y.rds") # september 2020
+contact_matrices_pico3 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_PICO3_10y.rds") # september 2020
+contact_matrices_pico4 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_PICO4_10y.rds") # february 2021
+contact_matrices_pico5 <- readRDS("inst/extdata/data/contact_matrices/Contactpatterns_PICO5_10y.rds") # june 2021
 
+# need to run this before running convert_contact_matrices()------
 age_dist <- c(
   0.10319920, 0.11620856, 0.12740219, 0.12198707, 0.13083463,
   0.14514332, 0.12092904, 0.08807406, 0.04622194
@@ -26,147 +29,62 @@ get_transmission_matrix <- function(x, contact_mat) {
   # output
   return(rtn)
 }
+# -----------------------------------------------------------------
 
-# February 2021
-february_2021 <- list()
-
-for (i in 1:200){
+# Define function to convert contact matrices --------------------
+convert_contact_matrices <- function(x){
+  # create empty list to store converted matrices
+  rtn <- list()
+  # loop over different realisations of contact matrices
+  var_names <- paste0("c_smt.", 1:200)
   
-  tmp <- contact_matrices_pico4 %>%
-    select(part_age, cnt_age, var_names[i]) %>%
-    mutate(contact_type = c(rep("all", 81),
-                            rep("community", 81),
-                            rep("household", 81))) %>%
-    filter(contact_type == "all") %>%
-    select(-contact_type) %>%
-    pivot_wider(., names_from = cnt_age, values_from = var_names[i]) %>%
-    select(-part_age)
+  for (i in 1:200){
+    
+    tmp <- x %>%
+      select(part_age, cnt_age, var_names[i]) %>%
+      mutate(contact_type = c(rep("all", 81),
+                              rep("community", 81),
+                              rep("household", 81))) %>%
+      filter(contact_type == "all") %>%
+      select(-contact_type) %>%
+      pivot_wider(., names_from = cnt_age, values_from = var_names[i]) %>%
+      select(-part_age)
+    
+    # convert to transmission matrix
+    tmp1 <- as.matrix(tmp) %*% N_diag
+    tmp2 <- get_transmission_matrix(rel_trans, tmp1)
+    
+    rtn[[i]] <- tmp2
+    
+  }
   
-  # convert to transmission matrix
-  tmp1 <- as.matrix(tmp) %*% N_diag
-  tmp2 <- get_transmission_matrix(rel_trans, tmp1)
+  # add mean to list of matrices
+  tmp_mean <- aaply(laply(rtn, as.matrix), c(2, 3), mean)
+  #tmp1_mean <- as.matrix(tmp_mean) %*% N_diag
+  #tmp2_mean <- get_transmission_matrix(rel_trans, tmp1_mean)
+  rtn$mean <- tmp_mean
   
-  february_2021[[i]] <- tmp
-
+  return(rtn)
 }
+# -----------------------------------------------------------------
 
-# add mean to list of matrices
-tmp_mean <- aaply(laply(february_2021, as.matrix), c(2, 3), mean)
-tmp1_mean <- as.matrix(tmp_mean) %*% N_diag
-tmp2_mean <- get_transmission_matrix(rel_trans, tmp1_mean)
-february_2021$mean <- tmp2_mean
- 
-# save contact matrices to file
-saveRDS(february_2021, file = "inst/extdata/data/contact_matrices_february_2021.rds")
+# convert contact matrices and save to file
+baseline_2017 <- convert_contact_matrices(contact_matrices_pienter3)
+saveRDS(baseline_2017, file = "inst/extdata/data/contact_matrices/contact_matrices_baseline_2017.rds")
 
-# June 2021
-june_2021 <- list()
+april_2020 <- convert_contact_matrices(contact_matrices_pico1)
+saveRDS(april_2020, file = "inst/extdata/data/contact_matrices/contact_matrices_april_2020.rds")
 
-for (i in 1:200){
-  
-  tmp <- contact_matrices_pico5 %>%
-    select(part_age, cnt_age, var_names[i]) %>%
-    mutate(contact_type = c(rep("all", 81),
-                            rep("community", 81),
-                            rep("household", 81))) %>%
-    filter(contact_type == "all") %>%
-    select(-contact_type) %>%
-    pivot_wider(., names_from = cnt_age, values_from = var_names[i]) %>%
-    select(-part_age)
-  
-  june_2021[[i]] <- tmp
-  
-}
+june_2020 <- convert_contact_matrices(contact_matrices_pico2)
+saveRDS(june_2020, file = "inst/extdata/data/contact_matrices/contact_matrices_june_2020.rds")
 
-# add mean to list of matrices
-june_2021$mean <- aaply(laply(june_2021, as.matrix), c(2, 3), mean)
+september_2020 <- convert_contact_matrices(contact_matrices_pico3)
+saveRDS(september_2020, file = "inst/extdata/data/contact_matrices/contact_matrices_september_2020.rds")
 
-# save contact matrices to file
-saveRDS(june_2021, file = "inst/extdata/data/contact_matrices_june_2021.rds")
+february_2021 <- convert_contact_matrices(contact_matrices_pico4)
+saveRDS(february_2021, file = "inst/extdata/data/contact_matrices/contact_matrices_february_2021.rds")
 
-
-### old code ### -----------------------------------------------
-# --------------------------------------------------------------
-
-# demographic info to make contact matrices symmetrical
-# population distributions
-# demo <- data.frame(age_group = c("[0,10)", "[10,20)", "[20,30)", "[30,40)","[40,50)", "[50,60)",
-#                                  "[60,70)", "[70,80)", "[80,Inf]"), 
-#                    frac_2017 = c(0.105, 0.118, 0.126, 0.120, 0.138, 0.145, 0.122, 0.0808, 0.0447), 
-#                    frac_2019 = c(0.103, 0.116, 0.127, 0.122, 0.131, 0.145, 0.121, 0.0881, 0.0462),
-#                    frac_2020 = c(0.102, 0.115, 0.128, 0.123, 0.127, 0.145, 0.121, 0.0904, 0.0472))
-
-# baseline (2017)
-# baseline <- contact_matrices_all %>%
-#   filter(survey == "baseline") %>%
-#   filter(contact_type == "all") %>%
-#   mutate(f_pop = rep(demo$frac_2017,9),
-#          c_est = round(m_est/f_pop,1)) 
-# 
-# baseline_asym <- baseline %>%
-#   select(-survey, -contact_type, -c_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = m_est)
-# 
-# baseline_sym <- baseline %>%
-#   select(-survey, -contact_type, -m_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = c_est)
-# 
-# # April 2020 (lockdown)
-# april2020 <- contact_matrices_all %>%
-#   filter(survey == "April 2020") %>%
-#   filter(contact_type == "all") %>%
-#   mutate(f_pop = rep(demo$frac_2019,9),
-#          c_est = round(m_est/f_pop,1))
-# 
-# april2020_asym <- april2020 %>%
-#   select(-survey, -contact_type, -c_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = m_est)
-# 
-# april2020_sym <- april2020 %>%
-#   select(-survey, -contact_type, -m_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = c_est)
-# 
-# # June 2020 
-# june2020 <- contact_matrices_all %>%
-#   filter(survey == "June 2020") %>%
-#   filter(contact_type == "all") %>%
-#   mutate(f_pop = rep(demo$frac_2019,9),
-#          c_est = round(m_est/f_pop,1))
-# 
-# june2020_asym <- june2020 %>%
-#   select(-survey, -contact_type, -c_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = m_est)
-# 
-# june2020_sym <- june2020 %>%
-#   select(-survey, -contact_type, -m_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = c_est)
-# 
-# # September 2020 
-# september2020 <- contact_matrices_all %>%
-#   filter(survey == "September 2020") %>%
-#   filter(contact_type == "all") %>%
-#   mutate(f_pop = rep(demo$frac_2019,9),
-#          c_est = round(m_est/f_pop,1)) 
-# 
-# september2020_asym <- september2020 %>%
-#   select(-survey, -contact_type, -c_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = m_est)
-# 
-# september2020_sym <- september2020 %>%
-#   select(-survey, -contact_type, -m_est, -f_pop) %>%
-#   pivot_wider(., names_from = cont_age, values_from = c_est)
-
-# put in a list and write to rds
-# cm <- list(baseline_sym = baseline_sym,
-#            baseline_asym = baseline_asym,
-#            april2020_sym = april2020_sym,
-#            april2020_asym = april2020_asym,
-#            june2020_sym = june2020_sym,
-#            june2020_asym = june2020_asym,
-#            september2020_sym = september2020_sym,
-#            september2020_asym = september2020_asym,
-#            february2021_sym = february2021_sym,
-#            february2021_asym = february2021_asym
-# )
-# saveRDS(cm,"inst/extdata/data/contact_matrices_for_model_input.rds")
+june_2021 <- convert_contact_matrices(contact_matrices_pico5)
+saveRDS(june_2021, file = "inst/extdata/data/contact_matrices/contact_matrices_june_2021.rds")
+# -----------------------------------------------------------------
 
