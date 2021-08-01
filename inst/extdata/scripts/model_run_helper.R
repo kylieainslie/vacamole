2db1685db415f955c192e6473675e9980c712974
Kylie Ainslie <kylie.ainslie@rivm.nl>
2021-08-01 10:04:40 +0200

    comment out initial states for Feb 1

--- inst/extdata/scripts/model_run_helper.R
+++ inst/extdata/scripts/model_run_helper.R
@@ -80,53 +80,6 @@ beta <- (r0 / rho) * g
 K <- (1 / g) * beta * S %*% baseline_2017$mean
 as.numeric(eigs(K, 1)$values) # this should be r0
 
-days <- seq(yday(as.Date("2021-01-31")), yday(as.Date("2021-12-31")), by = 1)
-breakpoints <- yday(c(
-  as.Date("2021-02-28"),
-  as.Date("2021-03-21"),
-  as.Date("2021-04-01"),
-  as.Date("2021-04-08"),
-  as.Date("2021-04-15"),
-  as.Date("2021-04-22"),
-  as.Date("2021-04-30"),
-  as.Date("2021-05-25")
-))
-beta_mle_vals <- c(0.00045, 0.00038, 0.00041, 0.00052, 0.00057, 0.00061, 0.00062, 0.00061)
-betas <- c(
-  rep(beta_mle_vals[1], length(days[1]:breakpoints[1])),
-  rep(beta_mle_vals[2], length(breakpoints[1]:breakpoints[2]) - 1),
-  rep(beta_mle_vals[3], length(breakpoints[2]:breakpoints[3]) - 1),
-  rep(beta_mle_vals[4], length(breakpoints[3]:breakpoints[4]) - 1),
-  rep(beta_mle_vals[5], length(breakpoints[4]:breakpoints[5]) - 1),
-  rep(beta_mle_vals[6], length(breakpoints[5]:breakpoints[6]) - 1),
-  rep(beta_mle_vals[7], length(breakpoints[6]:breakpoints[7]) - 1),
-  rep(beta_mle_vals[8], length(breakpoints[7]:days[length(days)]) - 1)
-)
-beta_t_ <- betas * (1 + 0.15 * cos(2 * pi * days / 365.24))
-R0 <- (beta_t_ / g) * rho
-R0_dat <- date <- data.frame(
-  date = seq.Date(as.Date("2021-01-31"), as.Date("2021-12-31"), by = 1),
-  R0 = R0,
-  roll_mean_R0 = zoo::rollmean(R0, k = 7, fill = NA)
-)
-r0_plot <- ggplot(data = R0_dat, aes(x = date, y = roll_mean_R0)) +
-  geom_line() +
-  labs(y = "Basic reproduction number (R0)", x = "Date") +
-  theme( # legend.position = "bottom",
-    panel.background = element_blank(),
-    axis.text = element_text(size = 14),
-    axis.text.x = element_text(size = 14),
-    axis.text.y = element_text(size = 14)
-  )
-r0_plot
-
-# ggsave("inst/extdata/results/r0_plot_16june.jpg",
-#   plot = r0_plot,
-#   # height = 8,
-#   # width = 12,
-#   dpi = 300
-# )
-
 # define state transition rates ------------------------------------
 h <- p_infection2admission / time_symptom2admission
 i1 <- p_admission2IC / time_admission2IC
@@ -173,88 +126,88 @@ h_multiplier <- list(
 )
 # initial states ---------------------------------------------------
 # Jacco's suggested way to determine initial conditions
-init_states_dat <- data.frame(
-  age_group = c(
-    "0-9", "10-19", "20-29", "30-39", "40-49",
-    "50-59", "60-69", "70-79", "80+"
-  ),
-  n = n_vec,
-  # from Scott
-  n_recovered = c(
-    30720, 397100, 642600, 419000, 412200, 505900,
-    349100, 206800, 115900 + 33200
-  ),
-  # from sitrep for 26 januari tot 2 februari:
-  # https://www.rivm.nl/coronavirus-covid-19/actueel/wekelijkse-update-epidemiologische-situatie-covid-19-in-nederland)
-  n_cases = c(
-    835, 2851, 4591, 3854, 3925, 5191, 3216, 1819,
-    1376 + 485
-  ),
-  # from NICE data (n_hosp/n_ic refers to occupancy on 1 Feb 2021)
-  n_hosp = c(2, 1, 8, 19, 29, 76, 142, 159, 186),
-  n_ic = c(0, 2, 6, 9, 25, 83, 181, 150, 12),
-  # from NICE data: people with length of stay >= 9 days
-  n_hosp_after_ic = c(2, 2, 9, 15, 45, 158, 321, 392, 266)
-) %>%
-  mutate(
-    n_infections = n_cases * 2.2, # calibrated to match osiris data
-    init_E = n_infections * (2 / 7),
-    init_I = n_infections * (2 / 7),
-    init_S = n - n_recovered - init_E - init_I - n_hosp - n_ic - n_hosp_after_ic
-  )
-
-# determine transmission rate for reff ------------------------------
-reff <- 1.04 # from RIVM open data for 1 Feb 2021 (midpoint between
-#              0.94 (wt) and 1.13 (UK variant))
-S2 <- diag(init_states_dat$init_S)
-rho2 <- as.numeric(eigs(S2 %*% t5, 1)$values)
-beta2 <- reff / rho2 * g
-# check
-B <- t5
-K2 <- beta2 * (1 / g) * S2 %*% B
-as.numeric(eigs(K2, 1)$values) # this should be r0
-
-# callibrate distribution of cases across age groups ----------------
-w <- eigen(K2)$vectors[, 1] / sum(eigen(K2)$vectors[, 1]) # should match dist_cases (I think!)
-x <- init_states_dat$n_cases / sum(init_states_dat$n_cases)
-A <- diag(x / w)
-B_prime <- A %*% B
-rho2_prime <- as.numeric(eigs(S2 %*% B_prime, 1)$values)
-beta2_prime <- reff / rho2_prime * g
-K2_prime <- beta2_prime * (1 / g) * S2 %*% B_prime
-dom_eig_vec <- eigen(K2_prime)$vectors[, 1]
-w_prime <- dom_eig_vec / sum(dom_eig_vec)
-as.numeric(eigs(K2_prime, 1)$values)
-
-# Specify initial values -------------------------------------------
-empty_state <- c(rep(0, 9))
-init <- c(
-  t = 0,
-  S = init_states_dat$init_S,
-  Shold_1d = empty_state,
-  Sv_1d = empty_state,
-  Shold_2d = empty_state,
-  Sv_2d = empty_state,
-  E = init_states_dat$init_E,
-  Ev_1d = empty_state,
-  Ev_2d = empty_state,
-  I = init_states_dat$init_I,
-  Iv_1d = empty_state,
-  Iv_2d = empty_state,
-  H = init_states_dat$n_hosp,
-  Hv_1d = empty_state,
-  Hv_2d = empty_state,
-  H_IC = init_states_dat$n_hosp_after_ic,
-  H_ICv_1d = empty_state,
-  H_ICv_2d = empty_state,
-  IC = init_states_dat$n_ic,
-  ICv_1d = empty_state,
-  ICv_2d = empty_state,
-  D = empty_state,
-  R = init_states_dat$n_recovered,
-  Rv_1d = empty_state,
-  Rv_2d = empty_state
-)
+# init_states_dat <- data.frame(
+#   age_group = c(
+#     "0-9", "10-19", "20-29", "30-39", "40-49",
+#     "50-59", "60-69", "70-79", "80+"
+#   ),
+#   n = n_vec,
+#   # from Scott
+#   n_recovered = c(
+#     30720, 397100, 642600, 419000, 412200, 505900,
+#     349100, 206800, 115900 + 33200
+#   ),
+#   # from sitrep for 26 januari tot 2 februari:
+#   # https://www.rivm.nl/coronavirus-covid-19/actueel/wekelijkse-update-epidemiologische-situatie-covid-19-in-nederland)
+#   n_cases = c(
+#     835, 2851, 4591, 3854, 3925, 5191, 3216, 1819,
+#     1376 + 485
+#   ),
+#   # from NICE data (n_hosp/n_ic refers to occupancy on 1 Feb 2021)
+#   n_hosp = c(2, 1, 8, 19, 29, 76, 142, 159, 186),
+#   n_ic = c(0, 2, 6, 9, 25, 83, 181, 150, 12),
+#   # from NICE data: people with length of stay >= 9 days
+#   n_hosp_after_ic = c(2, 2, 9, 15, 45, 158, 321, 392, 266)
+# ) %>%
+#   mutate(
+#     n_infections = n_cases * 2.2, # calibrated to match osiris data
+#     init_E = n_infections * (2 / 7),
+#     init_I = n_infections * (2 / 7),
+#     init_S = n - n_recovered - init_E - init_I - n_hosp - n_ic - n_hosp_after_ic
+#   )
+# 
+# # determine transmission rate for reff ------------------------------
+# reff <- 1.04 # from RIVM open data for 1 Feb 2021 (midpoint between
+# #              0.94 (wt) and 1.13 (UK variant))
+# S2 <- diag(init_states_dat$init_S)
+# rho2 <- as.numeric(eigs(S2 %*% t5, 1)$values)
+# beta2 <- reff / rho2 * g
+# # check
+# B <- t5
+# K2 <- beta2 * (1 / g) * S2 %*% B
+# as.numeric(eigs(K2, 1)$values) # this should be r0
+# 
+# # callibrate distribution of cases across age groups ----------------
+# w <- eigen(K2)$vectors[, 1] / sum(eigen(K2)$vectors[, 1]) # should match dist_cases (I think!)
+# x <- init_states_dat$n_cases / sum(init_states_dat$n_cases)
+# A <- diag(x / w)
+# B_prime <- A %*% B
+# rho2_prime <- as.numeric(eigs(S2 %*% B_prime, 1)$values)
+# beta2_prime <- reff / rho2_prime * g
+# K2_prime <- beta2_prime * (1 / g) * S2 %*% B_prime
+# dom_eig_vec <- eigen(K2_prime)$vectors[, 1]
+# w_prime <- dom_eig_vec / sum(dom_eig_vec)
+# as.numeric(eigs(K2_prime, 1)$values)
+# 
+# # Specify initial values -------------------------------------------
+# empty_state <- c(rep(0, 9))
+# init <- c(
+#   t = 0,
+#   S = init_states_dat$init_S,
+#   Shold_1d = empty_state,
+#   Sv_1d = empty_state,
+#   Shold_2d = empty_state,
+#   Sv_2d = empty_state,
+#   E = init_states_dat$init_E,
+#   Ev_1d = empty_state,
+#   Ev_2d = empty_state,
+#   I = init_states_dat$init_I,
+#   Iv_1d = empty_state,
+#   Iv_2d = empty_state,
+#   H = init_states_dat$n_hosp,
+#   Hv_1d = empty_state,
+#   Hv_2d = empty_state,
+#   H_IC = init_states_dat$n_hosp_after_ic,
+#   H_ICv_1d = empty_state,
+#   H_ICv_2d = empty_state,
+#   IC = init_states_dat$n_ic,
+#   ICv_1d = empty_state,
+#   ICv_2d = empty_state,
+#   D = empty_state,
+#   R = init_states_dat$n_recovered,
+#   Rv_1d = empty_state,
+#   Rv_2d = empty_state
+# )
 
 # read in vac schedules --------------------------------------------
 basis <- read_csv("inst/extdata/data/vaccination_scenarios/Cum_upt20210701 BASIS 75% in 12+ KA.csv") %>%
@@ -321,7 +274,7 @@ basis1 <- convert_vac_schedule(
 
 
 # load model fits --------------------------------------------------
-daily_cases_from_fit <- readRDS("inst/extdata/results/model_fit_df_2021-05-25.rds")
+#daily_cases_from_fit <- readRDS("inst/extdata/results/model_fit_df_2021-05-25.rds")
 # mle_betas <- read_csv("inst/extdata/results/mle_betas_2021-04-26.csv")
-initial_conditions <- readRDS("inst/extdata/results/init_conditions_2021-05-25.rds")
+#initial_conditions <- readRDS("inst/extdata/results/init_conditions_2021-05-25.rds")
 # osiris_dat <- readRDS("inst/extdata/data/Osiris_Data_20210505_1034.rds")
