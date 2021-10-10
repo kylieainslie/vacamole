b0b4a5c21e6de90c60bafaa46cd12ee5edc4a4ca
kylieainslie <ainslie.kylie@gmail.com>
2021-10-10 21:44:50 +0200

    remove unneccessary files from repo

--- inst/extdata/depricated/rep_cpp.h
+++
@@ -1,9 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-#ifndef REP_CPP_H
-#define REP_CPP_H
-
-NumericVector rep_cpp(NumericVector x, NumericVector y);
-
-#endif
--- inst/extdata/depricated/waning_script.R
+++
@@ -1,5 +0,0 @@
-# looking at waning rates
-
-t_vec <- seq(0, 365, by = 0.1)
-waning <- 1 - (1 / (1 + exp(-0.05 * (t_vec - 180))))
-plot(t_vec,waning,type = "l")
