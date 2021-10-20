73b687201dae877d004897b64410d12368d1bf20
kylieainslie <ainslie.kylie@gmail.com>
2021-10-20 08:40:18 +0200

    create data for vignette

---
+++ inst/extdata/scripts/helpers/read_case_data_from_server.R
@@ -0,0 +1,38 @@
+# ----------------------------------------------------
+# read in updated OSIRIS data from RIVM server
+# ----------------------------------------------------
+
+# get file path --------------------------------------
+path <- "/rivm/r/COVID-19/Surveillance/Data/OSIRIS/Geschoond/"
+file <- list.files(path, pattern = ".rds")
+if (identical(file, character(0))) {
+  path <- paste0(path,"Previous/")
+  file <- list.files(path, pattern = ".rds") %>%
+    max()
+}
+
+# read in data ---------------------------------------
+osiris <- readRDS(paste0(path,file))
+
+# aggregate for number of cases per day --------------
+# this removes any identifiable data
+
+osiris_tally <- osiris %>%
+  select(OSIRISNR, INFECTIEZIEKTE, ZIE1eZiekteDt, Land) %>%
+  filter(Land == "Nederland",
+         INFECTIEZIEKTE %in% c("NCOV", "Weak Positive", "Antitgen Pos. + Symptoms", "PCR Positief", "Antigen Positief")) %>%
+  select(-Land) %>%
+  rename(date = ZIE1eZiekteDt) %>%
+  group_by(date) %>%
+  summarise(inc = n()) %>%
+  filter(!is.na(date)) %>%
+  complete(date = seq.Date(min(date), max(date), by="day"), fill = list(inc = 0))
+
+# calculate rolling average and remove last 3 days ---
+osiris1 <- osiris_tally %>%
+  mutate(roll_avg = zoo::rollmean(inc, k = 7, fill = 0)) %>%
+  filter(date < max(date)-2) # remove last 3 days due to reporting delay
+
+last_date_in_osiris <- tail(osiris1$date, 1)
+# save output ----------------------------------------
+saveRDS(osiris1, file = paste0("inst/extdata/data/case_data_upto_", last_date_in_osiris, ".rds"))
