a02931bbe52b39056d1b56b7c5a31b8b57178f41
kylieainslie <ainslie.kylie@gmail.com>
2021-10-21 12:14:42 +0200

    build vignettes

---
+++ doc/quick_start_guide.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+#library(vacamole)
+
+## ----load_data, echo=FALSE, message=FALSE-------------------------------------
+# Load required packages ---------------------------------------
+# library(dplyr)
+# library(knitr)
+
+
