efc8e616f5cec966a8971a1cd12ae401d3ac780d
kylieainslie <ainslie.kylie@gmail.com>
2021-10-21 12:00:49 +0200

    create separate function files for converting contact matrix to transmission matrix

---
+++ R/get_transmission_matrix.R
@@ -0,0 +1,14 @@
+#' Convert contact matrix to transmission matrix
+#' @param x vector of relative susceptibility and infectiousnes for each age group
+#' @param contact_mat contact matrix
+#' @return transmission matrix
+#' @keywords vacamole
+#' @export
+get_transmission_matrix <- function(x, contact_mat) {
+  # multiply by relative susc/inf
+  tmp <- sweep(contact_mat, 1, x, "*") # rows
+  rtn <- sweep(tmp, 2, x, "*") # columns
+  
+  # output
+  return(rtn)
+}
