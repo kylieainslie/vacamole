ac299e6751d42c47ab4e1eb9f1807ce3ec26e5a1
kylieainslie <ainslie.kylie@gmail.com>
2022-03-21 12:36:01 +0100

    CODE: initialize a main script for scenario hub modelling

---
+++ inst/extdata/scripts/scenario_hub/main_script.R
@@ -0,0 +1,45 @@
+# Script for running scenarios for the Eupropean Scenario Hub
+# URL: https://github.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe#readme
+
+# preamble ---------------------------------------------------------
+# This script will load necessary packages, data sources, fit the model to data,
+# and then run scenarios.
+# All scenarios will be using Dutch data
+# TODO: generalize to other European countries
+# TODO: break up code into work chunks
+# ------------------------------------------------------------------
+
+# Load required packages/functions ---------------------------------
+library(deSolve)
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(readxl)
+library(rARPACK)
+library(readr)
+library(lubridate)
+# -------------------------------------------------------------------
+
+# Load data ---------------------------------------------------------
+# read in transition rates
+transition_rates <- readRDS("inst/extdata/inputs/transition_rates.rds")
+
+# define population size (by age group)
+age_dist <- c(0.10319920, 0.11620856, 0.12740219, 0.12198707, 
+              0.13083463,0.14514332, 0.12092904, 0.08807406, 
+              0.04622194)
+n <- 17407585 # Dutch population size
+n_vec <- n * age_dist
+
+# contact matrices --------------------------------------------------
+path <- "/rivm/s/ainsliek/data/contact_matrices/converted/"
+
+april_2017     <- readRDS(paste0(path,"transmission_matrix_april_2017.rds"))
+april_2020     <- readRDS(paste0(path,"transmission_matrix_april_2020.rds"))
+june_2020      <- readRDS(paste0(path,"transmission_matrix_june_2020.rds"))
+september_2020 <- readRDS(paste0(path,"transmission_matrix_september_2020.rds"))
+february_2021  <- readRDS(paste0(path,"transmission_matrix_february_2021.rds"))
+june_2021      <- readRDS(paste0(path,"transmission_matrix_june_2021.rds"))
+november_2021  <- readRDS(paste0(path,"transmission_matrix_november_2021.rds"))
