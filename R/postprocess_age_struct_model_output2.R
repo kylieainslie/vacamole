df9bdb6bf5439549387a69f94435a59db185c0fe
kylieainslie <ainslie.kylie@gmail.com>
2022-05-06 10:58:04 +0200

    CODE: add post-processing for compartments for 4th and 5th doses and extra waning

--- R/postprocess_age_struct_model_output2.R
+++ R/postprocess_age_struct_model_output2.R
@@ -7,6 +7,7 @@
 #' @importFrom stringr str_detect
 #' @export
 postprocess_age_struct_model_output2 <- function(dat) {
+  # S ----------
   S <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "S[:digit:]")])
   Shold_1d <- dat %>%
@@ -21,6 +22,16 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "Shold_3d[:digit:]")])
   Sv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "Sv_3d[:digit:]")])
+  Shold_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Shold_4d[:digit:]")])
+  Sv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Sv_4d[:digit:]")])
+  Shold_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Shold_5d[:digit:]")])
+  Sv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Sv_5d[:digit:]")])
+  
+  # E ----------
   E <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "E[:digit:]")])
   Ev_1d <- dat %>%
@@ -29,6 +40,12 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "Ev_2d[:digit:]")])
   Ev_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "Ev_3d[:digit:]")])
+  Ev_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Ev_4d[:digit:]")])
+  Ev_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Ev_5d[:digit:]")])
+  
+  # I ----------
   I <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "I[:digit:]")])
   Iv_1d <- dat %>%
@@ -37,6 +54,12 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "Iv_2d[:digit:]")])
   Iv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "Iv_3d[:digit:]")])
+  Iv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Iv_4d[:digit:]")])
+  Iv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Iv_5d[:digit:]")])
+  
+  # H ----------
   H <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "H[:digit:]")])
   Hv_1d <- dat %>%
@@ -45,6 +68,12 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "Hv_2d[:digit:]")])
   Hv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "Hv_3d[:digit:]")])
+  Hv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Hv_4d[:digit:]")])
+  Hv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Hv_5d[:digit:]")])
+  
+  # H_IC -------
   H_IC <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "H_IC[:digit:]")])
   H_ICv_1d <- dat %>%
@@ -53,6 +82,12 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "H_ICv_2d[:digit:]")])
   H_ICv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "H_ICv_3d[:digit:]")])
+  H_ICv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "H_ICv_4d[:digit:]")])
+  H_ICv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "H_ICv_5d[:digit:]")])
+  
+  # IC ---------
   IC <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "IC[:digit:]")]) %>%
     select(-names(dat)[str_detect(names(dat), pattern = "H_IC[:digit:]")])
@@ -65,8 +100,18 @@ postprocess_age_struct_model_output2 <- function(dat) {
   ICv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "ICv_3d[:digit:]")]) %>%
     select(-names(dat)[str_detect(names(dat), pattern = "H_ICv_3d[:digit:]")])
+  ICv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "ICv_4d[:digit:]")]) %>%
+    select(-names(dat)[str_detect(names(dat), pattern = "H_ICv_4d[:digit:]")])
+  ICv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "ICv_5d[:digit:]")]) %>%
+    select(-names(dat)[str_detect(names(dat), pattern = "H_ICv_5d[:digit:]")])
+  
+  # D ----------
   D <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "D[:digit:]")])
+  
+  # R ----------
   R <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "R[:digit:]")])
   Rv_1d <- dat %>%
@@ -75,6 +120,24 @@ postprocess_age_struct_model_output2 <- function(dat) {
     select(names(dat)[str_detect(names(dat), pattern = "Rv_2d[:digit:]")])
   Rv_3d <- dat %>%
     select(names(dat)[str_detect(names(dat), pattern = "Rv_3d[:digit:]")])
+  Rv_4d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_4d[:digit:]")])
+  Rv_5d <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_5d[:digit:]")])
+  
+  # R_1w ------
+  R_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "R_1w[:digit:]")])
+  Rv_1d_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_1d_1w[:digit:]")])
+  Rv_2d_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_2d_1w[:digit:]")])
+  Rv_3d_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_3d_1w[:digit:]")])
+  Rv_4d_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_4d_1w[:digit:]")])
+  Rv_5d_1w <- dat %>%
+    select(names(dat)[str_detect(names(dat), pattern = "Rv_5d_1w[:digit:]")])
 
   rtn <- list(
     S = S,
@@ -84,31 +147,53 @@ postprocess_age_struct_model_output2 <- function(dat) {
     Sv_2d = Sv_2d,
     Shold_3d = Shold_3d,
     Sv_3d = Sv_3d,
+    Shold_4d = Shold_4d,
+    Sv_4d = Sv_4d,
+    Shold_5d = Shold_5d,
+    Sv_5d = Sv_5d,
     E = E,
     Ev_1d = Ev_1d,
     Ev_2d = Ev_2d,
     Ev_3d = Ev_3d,
+    Ev_4d = Ev_4d,
+    Ev_5d = Ev_5d,
     I = I,
     Iv_1d = Iv_1d,
     Iv_2d = Iv_2d,
     Iv_3d = Iv_3d,
+    Iv_4d = Iv_4d,
+    Iv_5d = Iv_5d,
     H = H,
     Hv_1d = Hv_1d,
     Hv_2d = Hv_2d,
     Hv_3d = Hv_3d,
+    Hv_4d = Hv_4d,
+    Hv_5d = Hv_5d,
     H_IC = H_IC,
     H_ICv_1d = H_ICv_1d,
     H_ICv_2d = H_ICv_2d,
     H_ICv_3d = H_ICv_3d,
+    H_ICv_4d = H_ICv_4d,
+    H_ICv_5d = H_ICv_5d,
     IC = IC,
     ICv_1d = ICv_1d,
     ICv_2d = ICv_2d,
     ICv_3d = ICv_3d,
+    ICv_4d = ICv_4d,
+    ICv_5d = ICv_5d,
     D = D,
     R = R,
     Rv_1d = Rv_1d,
     Rv_2d = Rv_2d,
-    Rv_3d = Rv_3d
+    Rv_3d = Rv_3d,
+    Rv_4d = Rv_4d,
+    Rv_5d = Rv_5d,
+    R_1w = R_1w,
+    Rv_1d_1w = Rv_1d_1w,
+    Rv_2d_1w = Rv_2d_1w,
+    Rv_3d_1w = Rv_3d_1w,
+    Rv_4d_1w = Rv_4d_1w,
+    Rv_5d_1w = Rv_5d_1w
   )
 
   return(rtn)
