110c82559ac40aae35b0f9f1452f869afce9977d
kylieainslie <ainslie.kylie@gmail.com>
2022-05-30 16:16:50 +0200

    CODE: update file format of osiris case data .rds -> .fst

--- inst/extdata/scripts/helpers/read_case_data_from_server.R
+++ inst/extdata/scripts/helpers/read_case_data_from_server.R
@@ -1,18 +1,18 @@
 # ----------------------------------------------------
 # read in updated OSIRIS data from RIVM server
 # ----------------------------------------------------
-
+library(fst)
 # get file path --------------------------------------
 path <- "/rivm/r/COVID-19/Surveillance/Data/OSIRIS/Geschoond/"
-file <- list.files(path, pattern = ".rds")
+file <- list.files(path, pattern = ".fst")
 if (identical(file, character(0))) {
   path <- paste0(path,"Previous/")
-  file <- list.files(path, pattern = ".rds") %>%
+  file <- list.files(path, pattern = ".fst") %>%
     max()
 }
 
 # read in data ---------------------------------------
-osiris <- readRDS(paste0(path,file))
+osiris <- read_fst(paste0(path,file))
 
 # aggregate for number of cases per day --------------
 # this removes any identifiable data
@@ -31,7 +31,7 @@ osiris_tally <- osiris %>%
 # calculate rolling average and remove last 3 days ---
 osiris1 <- osiris_tally %>%
   mutate(roll_avg = zoo::rollmean(inc, k = 7, fill = 0)) %>%
-  filter(date < max(date)-2) # remove last 3 days due to reporting delay
+  filter(date <= as.Date("2022-05-21"))#max(date)-2) # remove last 3 days due to reporting delay
 
 last_date_in_osiris <- tail(osiris1$date, 1)
 # save output ----------------------------------------
